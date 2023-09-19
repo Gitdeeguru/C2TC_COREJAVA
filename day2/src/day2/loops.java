8ba9a86d8d8ad43cfa2e92fda5c45ba5c901d0a7
deepak <deepak1ga20cs401@gmail.com>
2023-09-19 18:21:50 +0530

    daytwo

---
+++ day2/src/day2/loops.java
@@ -0,0 +1,36 @@
+package day2;
+
+public class loops {
+
+	public static void main(String[] args) {
+		
+		
+		//for loop
+//		int n=10;
+//		int count=0;
+//		for (int i=0 ; i<=n ; ++i) {
+//			System.out.println(count + "." + " Hello world");
+//			count++;
+//		}
+		
+		
+		
+		//while loop
+//		int i = 0;
+//		while(i<10) 
+//		{
+//			System.out.println(i);
+//			i++;
+//		}
+//		
+		
+		
+		//Do-while loop
+		int i = 0;
+		do {
+			System.out.println(i);
+			i++;
+		}while(i <= 10);
+	}
+
+}
